d78dc8aa36d391e8b8505124cfc2d35868f3c7b9
3ndrius <andrzejo1991@gmail.com>
2019-10-17 22:41:05 +0200

    Display product from file

--- dist/VanillaJSShop.77de5100.js
+++ dist/VanillaJSShop.77de5100.js
@@ -116,7 +116,7 @@ module.exports = {
       "image": {
         "fields": {
           "file": {
-            "url": "./assets/berlin.jpg"
+            "url": "/egipt.8a5a444c.jpg"
           }
         }
       }
@@ -131,7 +131,7 @@ module.exports = {
       "image": {
         "fields": {
           "file": {
-            "url": "./assets/madrid.jpg"
+            "url": "/egipt.8a5a444c.jpg"
           }
         }
       }
@@ -146,7 +146,7 @@ module.exports = {
       "image": {
         "fields": {
           "file": {
-            "url": "./assets/buenos.jpg"
+            "url": "/egipt.8a5a444c.jpg"
           }
         }
       }
@@ -161,7 +161,7 @@ module.exports = {
       "image": {
         "fields": {
           "file": {
-            "url": "./assets/sydney.jpg"
+            "url": "/egipt.8a5a444c.jpg"
           }
         }
       }
@@ -176,7 +176,7 @@ module.exports = {
       "image": {
         "fields": {
           "file": {
-            "url": "./assets/pekin.jpg"
+            "url": "/egipt.8a5a444c.jpg"
           }
         }
       }
@@ -191,7 +191,7 @@ module.exports = {
       "image": {
         "fields": {
           "file": {
-            "url": "./assets/moscow.jpg"
+            "url": "/egipt.8a5a444c.jpg"
           }
         }
       }
@@ -206,7 +206,7 @@ module.exports = {
       "image": {
         "fields": {
           "file": {
-            "url": "./assets/budapest.jpg"
+            "url": "/egipt.8a5a444c.jpg"
           }
         }
       }
@@ -221,7 +221,7 @@ module.exports = {
       "image": {
         "fields": {
           "file": {
-            "url": "./assets/london.jpg"
+            "url": "/egipt.8a5a444c.jpg"
           }
         }
       }
@@ -229,10 +229,154 @@ module.exports = {
   }]
 };
 },{}],"index.ts":[function(require,module,exports) {
+var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
+  function adopt(value) {
+    return value instanceof P ? value : new P(function (resolve) {
+      resolve(value);
+    });
+  }
+
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+var __generator = this && this.__generator || function (thisArg, body) {
+  var _ = {
+    label: 0,
+    sent: function sent() {
+      if (t[0] & 1) throw t[1];
+      return t[1];
+    },
+    trys: [],
+    ops: []
+  },
+      f,
+      y,
+      t,
+      g;
+  return g = {
+    next: verb(0),
+    "throw": verb(1),
+    "return": verb(2)
+  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
+    return this;
+  }), g;
+
+  function verb(n) {
+    return function (v) {
+      return step([n, v]);
+    };
+  }
+
+  function step(op) {
+    if (f) throw new TypeError("Generator is already executing.");
+
+    while (_) {
+      try {
+        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+        if (y = 0, t) op = [op[0] & 2, t.value];
+
+        switch (op[0]) {
+          case 0:
+          case 1:
+            t = op;
+            break;
+
+          case 4:
+            _.label++;
+            return {
+              value: op[1],
+              done: false
+            };
+
+          case 5:
+            _.label++;
+            y = op[1];
+            op = [0];
+            continue;
+
+          case 7:
+            op = _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+
+          default:
+            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+              _ = 0;
+              continue;
+            }
+
+            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
+              _.label = op[1];
+              break;
+            }
+
+            if (op[0] === 6 && _.label < t[1]) {
+              _.label = t[1];
+              t = op;
+              break;
+            }
+
+            if (t && _.label < t[2]) {
+              _.label = t[2];
+
+              _.ops.push(op);
+
+              break;
+            }
+
+            if (t[2]) _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+        }
+
+        op = body.call(thisArg, _);
+      } catch (e) {
+        op = [6, e];
+        y = 0;
+      } finally {
+        f = t = 0;
+      }
+    }
+
+    if (op[0] & 5) throw op[1];
+    return {
+      value: op[0] ? op[1] : void 0,
+      done: true
+    };
+  }
+};
+
 var cartMenuWrapper = document.querySelector('.shop-cart');
 var cartMenuBtn = document.querySelector('.cart-button');
 var cartMenu = document.querySelector('.shop-cart__container');
 var closeBtn = document.querySelector('.close');
+var productsDom = document.querySelector('.products__container');
 cartMenuBtn.addEventListener('click', function () {
   cartMenuWrapper.style.display = 'flex';
   cartMenu.style.transform = "translateX(0%)";
@@ -251,17 +395,87 @@ function () {
   function Products() {}
 
   Products.prototype.getProducts = function () {
-    var result = require('./products.json');
+    return __awaiter(this, void 0, void 0, function () {
+      var result, data, err_1;
+      return __generator(this, function (_a) {
+        switch (_a.label) {
+          case 0:
+            _a.trys.push([0, 2,, 3]);
 
-    console.log(result);
-    return result;
+            return [4
+            /*yield*/
+            , require('./products.json')];
+
+          case 1:
+            result = _a.sent();
+            data = result.items;
+            data = data.map(function (item) {
+              var _a = item.fields,
+                  title = _a.title,
+                  price = _a.price;
+              var id = item.sys.id;
+              var image = item.fields.image.fields.file.url;
+              return {
+                title: title,
+                price: price,
+                id: id,
+                image: image
+              };
+            });
+            return [2
+            /*return*/
+            , data];
+
+          case 2:
+            err_1 = _a.sent();
+            console.log(err_1);
+            return [3
+            /*break*/
+            , 3];
+
+          case 3:
+            return [2
+            /*return*/
+            ];
+        }
+      });
+    });
   };
 
   return Products;
 }();
 
-var product = new Products();
-product.getProducts();
+var UI =
+/** @class */
+function () {
+  function UI() {}
+
+  UI.prototype.showProducts = function (data) {
+    var resultItem = " ";
+    data.forEach(function (item) {
+      resultItem += "\n        <article class=\"products__item\">\n        <div class=\"products__image\">\n            <img src=" + item.image + " alt=\"products-image\">\n            <i class=\"fa fa-shopping-cart fa-lg\" data-id=" + item.id + " aria-hidden=\"true\"></i>\n        </div>\n        <div class=\"products__title\">\n            <h2>" + item.title + "</h2>\n        </div>\n        <div class=\"products__price\">\n            " + item.price + "\n        </div>\n    </article>\n        ";
+    });
+    productsDom.innerHTML = resultItem;
+  };
+
+  return UI;
+}();
+
+var Storage =
+/** @class */
+function () {
+  function Storage() {}
+
+  return Storage;
+}();
+
+document.addEventListener('DOMContentLoaded', function () {
+  var products = new Products();
+  var layout = new UI();
+  products.getProducts().then(function (product) {
+    layout.showProducts(product);
+  });
+});
 },{"./products.json":"products.json"}],"C:/Users/andrz/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -289,7 +503,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62717" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65328" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
-
-
 const cartMenuWrapper = document.querySelector('.shop-cart');
 const cartMenuBtn = document.querySelector('.cart-button');
 const cartMenu = document.querySelector('.shop-cart__container');
 
 const closeBtn = document.querySelector('.close');
 
-cartMenuBtn.addEventListener('click', ()=> {
+const productsDom = document.querySelector('.products__container')
+
+cartMenuBtn.addEventListener('click', () => {
     cartMenuWrapper.style.display = 'flex';
     cartMenu.style.transform = "translateX(0%)"
     document.body.style.overflow = "hidden";
 })
 
-closeBtn.addEventListener('click', () =>{
+closeBtn.addEventListener('click', () => {
     cartMenuWrapper.style.display = 'none';
     cartMenu.style.transform = "translateX(101%)"
     document.body.style.overflowY = "scroll";
@@ -22,18 +22,69 @@ closeBtn.addEventListener('click', () =>{
 let cart = [];
 
 class Products {
-    
-   getProducts() {
-        
-           let result = require('./products.json');
-           console.log(result);
-            return result;
-       
-         
-       
+
+    async getProducts() {
+        try {
+            let result = await require('./products.json');
+            let data = result.items;
+            data = data.map(item => {
+                const {
+                    title,
+                    price
+                } = item.fields;
+                const {
+                    id
+                } = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {
+                    title,
+                    price,
+                    id,
+                    image
+                };
+            })
+            return data;
+        } catch (err) {
+            console.log(err);
+        }
+
     }
 }
+class UI {
 
-let product = new Products;
+    showProducts(data) {
+       let resultItem= " ";
 
-product.getProducts();
+       data.forEach(item => {
+        resultItem += `
+        <article class="products__item">
+        <div class="products__image">
+            <img src=${item.image} alt="products-image">
+            <i class="fa fa-shopping-cart fa-lg" data-id=${item.id} aria-hidden="true"></i>
+        </div>
+        <div class="products__title">
+            <h2>${item.title}</h2>
+        </div>
+        <div class="products__price">
+            ${item.price}
+        </div>
+    </article>
+        `;
+        
+       })
+       productsDom.innerHTML = resultItem;
+    }
+
+}
+class Storage {
+
+}
+document.addEventListener('DOMContentLoaded', () => {
+
+    let products = new Products;
+    let layout = new UI;
+    products.getProducts().then(product => {
+        layout.showProducts(product)
+    })
+  
+})
